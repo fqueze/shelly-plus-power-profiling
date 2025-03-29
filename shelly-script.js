0ba91629842e6dd1f2222b6ba1342f7aa62442c5
Florian Quèze <florian@queze.net>
2025-03-29 23:10:03 +0100

    Record and display voltage in addition to power values.

--- shelly-script.js
+++ shelly-script.js
@@ -1,5 +1,6 @@
 let startIndex = 0;
-let values = [];
+let powerValues = [];
+let voltageValues = [];
 let pending = false;
 let notifyTimer = Timer.set(1000, true, function() {
   if (pending)
@@ -10,25 +11,30 @@ let notifyTimer = Timer.set(1000, true, function() {
     function(res, err_code, err_msg, ud) {
       pending = false;
       if (res) {
-        if (values.length >= 600) {
-          values.splice(0, 1);
+        if (powerValues.length >= 300) {
+          powerValues.splice(0, 1);
+          voltageValues.splice(0, 1);
           ++startIndex;
         }
-        values.push(res.apower);
+        powerValues.push(res.apower);
+        voltageValues.push(res.voltage);
       }
     }
   );
 });
 
 HTTPServer.registerEndpoint("power", function(req, res) {
-  let data = {start_index: startIndex, power_values: values};
+  let data = {start_index: startIndex,
+              power_values: powerValues,
+              voltage_values: voltageValues};
   if (req.query) {
     let alreadyReceived = parseInt(req.query);
     if (alreadyReceived > startIndex &&
-        alreadyReceived <= startIndex + values.length) {
+        alreadyReceived <= startIndex + powerValues.length) {
       data = {
         start_index: alreadyReceived,
-        power_values: values.slice(alreadyReceived - startIndex)
+        power_values: powerValues.slice(alreadyReceived - startIndex),
+        voltage_values: voltageValues.slice(alreadyReceived - startIndex)
       };
     }
   }
@@ -37,13 +43,16 @@ HTTPServer.registerEndpoint("power", function(req, res) {
   if (req.query) {
     let alreadyReceived = parseInt(req.query);
     if (alreadyReceived > startIndex &&
-        alreadyReceived <= startIndex + values.length) {
+        alreadyReceived <= startIndex + powerValues.length) {
       let toRemove = alreadyReceived - startIndex
-      values.splice(0, toRemove);
+      powerValues.splice(0, toRemove);
+      voltageValues.splice(0, toRemove);
       startIndex += toRemove;
     }
   }
-  res.body = JSON.stringify({start_index: startIndex, power_values: values});
+  res.body = JSON.stringify({start_index: startIndex,
+                             power_values: powerValues,
+                             voltage_values: voltageValues});
 */
   res.headers = [["Access-Control-Allow-Origin", "*"],
                  ["Content-Type", "application/json"]];
