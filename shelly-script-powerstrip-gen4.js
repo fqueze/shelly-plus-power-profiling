0e97c2f031c54aeaf4b9738f4b1bcd8c20f9e755
Florian Quèze <florian@queze.net>
2025-10-12 23:18:03 +0200

    Make the scripts running on the device expiring oldest samples based on how much available memory remains.

--- shelly-script-powerstrip-gen4.js
+++ shelly-script-powerstrip-gen4.js
@@ -2,34 +2,62 @@ let startIndex = 0;
 let powerValues = [[], [], [], []];  // Array for each of the 4 switches
 let voltageValues = [];  // Voltage is typically shared
 let scriptId = Shelly.getCurrentScriptId();
+let requiredFreeMemory = 3 * 1024; // 3KB.
+let minFreeRatio = 0.3;
+let valueSize = 28; // 28 bytes of memory used per value we add to an array.
 
-let notifyTimer = Timer.set(1000, true, function() {
+function logMemory(message) {
+  if (message)
+    console.log(message);
   // Get system and script status synchronously
   let sysStatus = Shelly.getComponentStatus("sys");
   let scriptStatus = Shelly.getComponentStatus("script:" + scriptId);
-  console.log("sys", sysStatus.ram_size, sysStatus.ram_free, sysStatus.ram_min_free, scriptStatus.mem_used, scriptStatus.mem_peak, scriptStatus.mem_free);
+  console.log("sys", sysStatus.ram_size, sysStatus.ram_free, sysStatus.ram_min_free,
+              scriptStatus.mem_used, scriptStatus.mem_peak, scriptStatus.mem_free);
+}
+
+function canStoreOneMoreSample(valueCount) {
+  let sampleSize = valueCount * valueSize;
+  let status = Shelly.getComponentStatus("script:" + scriptId);
+  let totalSize = status.mem_used + status.mem_free;
+  let available = status.mem_free - sampleSize;
+  return available >= requiredFreeMemory && available >= totalSize * minFreeRatio;
+}
+
+let notifyTimer = Timer.set(1000, true, function() {
+  logMemory("timer");
+
+  // Check if we need to remove old data before adding new samples
+  // We're adding 5 values per timer: 4 power values + 1 voltage value
+  if (!canStoreOneMoreSample(5)) {
+    // Remove oldest samples from all arrays
+    for (let switchId = 0; switchId < 4; ++switchId) {
+      if (powerValues[switchId].length > 0) {
+        powerValues[switchId].splice(0, 1);
+      }
+    }
+    if (voltageValues.length > 0) {
+      voltageValues.splice(0, 1);
+      ++startIndex;
+    }
+  }
 
   // Collect data from all 4 switches synchronously
   for (let switchId = 0; switchId < 4; ++switchId) {
     let switchStatus = Shelly.getComponentStatus("switch:" + switchId);
-
-    if (powerValues[switchId].length >= 100) {
-      powerValues[switchId].splice(0, 1);
-      if (switchId === 0) {
-        voltageValues.splice(0, 1);
-        ++startIndex;
-      }
-    }
-
     powerValues[switchId].push(switchStatus.apower);
     if (switchId === 0) {
       // Store voltage only from first switch to avoid duplicates
       voltageValues.push(switchStatus.voltage);
     }
   }
+
+  logMemory("timer: done");
 });
 
 HTTPServer.registerEndpoint("power", function(req, res) {
+  logMemory("power request");
+
   let data = {
     start_index: startIndex,
     power_values: powerValues,
@@ -54,9 +82,13 @@ HTTPServer.registerEndpoint("power", function(req, res) {
       };
     }
   }
+  logMemory("power request: created data object");
 
   res.body = JSON.stringify(data);
+  logMemory("power request: serialized JSON");
+
   res.headers = [["Access-Control-Allow-Origin", "*"],
                  ["Content-Type", "application/json"]];
   res.send();
+  logMemory("power request: sent");
 });
--- shelly-script.js
+++ shelly-script.js
@@ -1,29 +1,46 @@
 let startIndex = 0;
 let powerValues = [];
 let voltageValues = [];
-let pending = false;
+let scriptId = Shelly.getCurrentScriptId();
+let requiredFreeMemory = 3 * 1024; // 3KB.
+let minFreeRatio = 0.3;
+let valueSize = 28; // 28 bytes of memory used per value we add to an array.
+
+function logMemory(message) {
+  if (message)
+    console.log(message);
+  // Get system and script status synchronously
+  let sysStatus = Shelly.getComponentStatus("sys");
+  let scriptStatus = Shelly.getComponentStatus("script:" + scriptId);
+  console.log("sys", sysStatus.ram_size, sysStatus.ram_free, sysStatus.ram_min_free,
+              scriptStatus.mem_used, scriptStatus.mem_peak, scriptStatus.mem_free);
+}
+
+function canStoreOneMoreSample(valueCount) {
+  let sampleSize = valueCount * valueSize;
+  let status = Shelly.getComponentStatus("script:" + scriptId);
+  let totalSize = status.mem_used + status.mem_free;
+  let available = status.mem_free - sampleSize;
+  console.log("canStoreOneMoreSample", status.mem_free, sampleSize, requiredFreeMemory, totalSize * minFreeRatio);
+  return available >= requiredFreeMemory && available >= totalSize * minFreeRatio;
+}
+
 let notifyTimer = Timer.set(1000, true, function() {
-  if (pending)
-    return;
-  pending = true;
-  Shelly.call(
-    "Switch.GetStatus", { id: 0 },
-    function(res, err_code, err_msg, ud) {
-      pending = false;
-      if (res) {
-        if (powerValues.length >= 300) {
-          powerValues.splice(0, 1);
-          voltageValues.splice(0, 1);
-          ++startIndex;
-        }
-        powerValues.push(res.apower);
-        voltageValues.push(res.voltage);
-      }
-    }
-  );
+  logMemory("timer");
+  let res = Shelly.getComponentStatus("switch:" + 0);
+  if (!canStoreOneMoreSample(2)) {
+    powerValues.splice(0, 1);
+    voltageValues.splice(0, 1);
+    ++startIndex;
+  }
+  powerValues.push(res.apower);
+  voltageValues.push(res.voltage);
+  logMemory("timer: done");
 });
 
 HTTPServer.registerEndpoint("power", function(req, res) {
+  logMemory("power request");
+
   let data = {start_index: startIndex,
               power_values: powerValues,
               voltage_values: voltageValues};
@@ -38,23 +55,13 @@ HTTPServer.registerEndpoint("power", function(req, res) {
       };
     }
   }
+  logMemory("power request: created data object");
+
   res.body = JSON.stringify(data);
-/* version that removes the old data once it has been received
-  if (req.query) {
-    let alreadyReceived = parseInt(req.query);
-    if (alreadyReceived > startIndex &&
-        alreadyReceived <= startIndex + powerValues.length) {
-      let toRemove = alreadyReceived - startIndex
-      powerValues.splice(0, toRemove);
-      voltageValues.splice(0, toRemove);
-      startIndex += toRemove;
-    }
-  }
-  res.body = JSON.stringify({start_index: startIndex,
-                             power_values: powerValues,
-                             voltage_values: voltageValues});
-*/
+  logMemory("power request: serialized JSON");
+
   res.headers = [["Access-Control-Allow-Origin", "*"],
                  ["Content-Type", "application/json"]];
   res.send();
+  logMemory("power request: sent");
 });
